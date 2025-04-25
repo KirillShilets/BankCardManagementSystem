233878413eabff9817160733344fe3fbe1dfb640
KirillShilets <164549163+KirillShilets@users.noreply.github.com>
2025-04-25 17:54:23 +0300

    fix: handling Forbidden(403) exception

--- src/main/java/com/testtask/bankcardmanager/exception/handler/ForbiddenAuthenticationEntryPoint.java
+++ src/main/java/com/testtask/bankcardmanager/exception/handler/ForbiddenAuthenticationEntryPoint.java
@@ -1,2 +1,54 @@
-package com.testtask.bankcardmanager.exception.handler;public class ForbiddenAuthenticationEntryPoint {
+package com.testtask.bankcardmanager.exception.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.testtask.bankcardmanager.dto.response.ErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Component
+public class ForbiddenAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(ForbiddenAuthenticationEntryPoint.class);
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public ForbiddenAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        logger.warn("Authentication required but Forbidden response requested for path: {}. Reason: {}",
+                request.getRequestURI(), authException.getMessage());
+        logger.debug("AuthenticationException details: ", authException);
+
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.FORBIDDEN.value(),
+                "Forbidden",
+                "Access Denied: Authentication is required and/or you do not have permissions.",
+                request.getRequestURI()
+        );
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        try (OutputStream os = response.getOutputStream()) {
+            objectMapper.writeValue(os, errorResponse);
+            os.flush();
+        }
+    }
 }
--- src/main/java/com/testtask/bankcardmanager/service/impl/AuthServiceImpl.java
+++ src/main/java/com/testtask/bankcardmanager/service/impl/AuthServiceImpl.java
@@ -1,2 +1,39 @@
-package com.testtask.bankcardmanager.service.impl;public class AuthServiceImpl {
+package com.testtask.bankcardmanager.service.impl;
+
+import com.testtask.bankcardmanager.dto.auth.request.LoginRequest;
+import com.testtask.bankcardmanager.dto.auth.response.JwtAuthenticationResponse;
+import com.testtask.bankcardmanager.security.jwt.JwtTokenProvider;
+import com.testtask.bankcardmanager.service.AuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthServiceImpl.class);
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider tokenProvider;
+
+    public AuthServiceImpl(AuthenticationManager authenticationManager, JwtTokenProvider tokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public JwtAuthenticationResponse login(LoginRequest loginRequest) {
+        logger.info("Attempting authentication for user: {}", loginRequest.getEmail());
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
+        );
+        logger.debug("Authentication successful for user: {}", loginRequest.getEmail());
+
+        String jwt = tokenProvider.generateToken(authentication);
+        logger.debug("JWT generated successfully for user: {}", loginRequest.getEmail());
+
+        return new JwtAuthenticationResponse(jwt);
+    }
 }
